9d735972ec6a0171e3d3015501546446ba16eaa7
NourMohammad Srouji <nourmohammadsrouji@NourMohammads-MacBook-Pro.local>
2025-06-10 11:08:20 +0300

    getting students' names and grades from user

--- Student-Grade-Analyzer.py
+++ Student-Grade-Analyzer.py
@@ -3,3 +3,8 @@ def display_student_summary(number_of_students):
     passed_students = 0
     names = []
     grades = []
+
+    for num in range(number_of_students):
+        student_name = input('Enter the name of the student: ')
+        grade = int(input('Enter the grade of the student: '))
+
