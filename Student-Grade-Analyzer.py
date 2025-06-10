0570f1c9fb0c1c8f2f4d1de61618e02fc54790df
NourMohammad Srouji <nourmohammadsrouji@NourMohammads-MacBook-Pro.local>
2025-06-10 11:20:18 +0300

    counting students who passed the test

--- Student-Grade-Analyzer.py
+++ Student-Grade-Analyzer.py
@@ -7,7 +7,10 @@ def display_student_summary(number_of_students):
     for num in range(number_of_students):
         student_name = input('Enter the name of the student: ')
         grade = int(input('Enter the grade of the student: '))
-
+        # testing the validation of the grade entered
         while grade < 0 or grade > 100 :
             print(grade, 'is invalid grade')
             grade = int(input('Please enter a new valid grade: '))
+        
+        if grade >= 60:
+            passed_students += 1
