8fcc7e2f42b58434c67283ff2f59a3d3db87e22e
NourMohammad Srouji <nourmohammadsrouji@NourMohammads-MacBook-Pro.local>
2025-06-10 11:17:09 +0300

    testing grades' validations, allowing user re-entering valid grade'

--- Student-Grade-Analyzer.py
+++ Student-Grade-Analyzer.py
@@ -8,3 +8,6 @@ def display_student_summary(number_of_students):
         student_name = input('Enter the name of the student: ')
         grade = int(input('Enter the grade of the student: '))
 
+        while grade < 0 or grade > 100 :
+            print(grade, 'is invalid grade')
+            grade = int(input('Please enter a new valid grade: '))
